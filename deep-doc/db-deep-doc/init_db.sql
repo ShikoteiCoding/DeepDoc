86832f691d200ae75c092e5065a8f0f6412a229d
Come Vitis <come.vitis@famoco.com>
2022-03-06 14:19:57 +0100

    Fixing docker compose pushing init_db to a different database name

--- deep-doc/db-deep-doc/init_db.sql
+++ deep-doc/db-deep-doc/init_db.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS piece (
+CREATE TABLE IF NOT EXISTS pieces (
     id INT NOT NULL,
     content TEXT NOT NULL,
     create_date TIMESTAMP WITH TIME ZONE NOT NULL,
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS piece (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS doc (
+CREATE TABLE IF NOT EXISTS docs (
     id INT NOT NULL,
     content text NOT NULL,
     create_date TIMESTAMP WITH TIME ZONE NOT NULL,
@@ -14,13 +14,13 @@ CREATE TABLE IF NOT EXISTS doc (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS doc_piece_rel (
-    piece_id INT NOT NULL,
-    doc_id INT NOT NULL,
-    CONSTRAINT fk_piece
-        FOREIGN KEY (piece_id) 
-            REFERENCES piece(id),
-    CONSTRAINT fk_doc
-        FOREIGN KEY (doc_id) 
-            REFERENCES doc(id)
+CREATE TABLE IF NOT EXISTS docs_pieces_rel (
+    pieces_id INT NOT NULL,
+    docs_id INT NOT NULL,
+    CONSTRAINT fk_pieces
+        FOREIGN KEY (pieces_id) 
+            REFERENCES pieces(id),
+    CONSTRAINT fk_docs
+        FOREIGN KEY (docs_id) 
+            REFERENCES docs(id)
 );
