da8f0885e6f6bc44c186ca87b681e6a05f6dcb12
Come Vitis <business.vitis@gmail.com>
2022-06-12 08:20:10 +0200

    Making it clearer in main file to avoid creating useless files for testing

--- deep-doc/src/main.py
+++ deep-doc/src/main.py
@@ -1,18 +1,32 @@
 from utils import (
-    Config, DBLayerAccess, 
-    DocumentParser, 
+    DBLayerAccess, 
     PieceMapper, DocumentMapper
 )
 
 from domain_types import Piece, Document
+
+from config import Config
+
+def usecase_create_and_push(db_layer: DBLayerAccess):
+    piece = Piece()
+
+    piece_mapper = PieceMapper(db_layer)
+    piece = Piece(**{"title": "Title", "content": "Content"})
+    inserted_piece = piece_mapper.insert(piece)
+
+    print(piece == inserted_piece)
+
+def usecase_create_nested_document(db_layer: DBLayerAccess):
+    PieceMapper(db_layer)
+    DocumentMapper(db_layer)
+    
 
 if __name__ == '__main__':
     
     c = Config()
     db_layer = DBLayerAccess(c)
     db_layer.connect()
-    piece_mapper = PieceMapper(db_layer)
-    doc_mapper = DocumentMapper(db_layer)
-    
+
+    usecase_create_and_push(db_layer)
 
     db_layer.close()
