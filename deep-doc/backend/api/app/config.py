0dc6899a6615dded648852f1ef3ce4a947180dca
Come Vitis <business.vitis@gmail.com>
2022-06-21 16:29:00 +0200

    Modifying config get from env logic

--- deep-doc/backend/api/app/config.py
+++ deep-doc/backend/api/app/config.py
@@ -6,16 +6,17 @@ class MissingEnvironmentVar(Exception):
     pass
 
 #
-def get_required(var: str) -> str:
-    try:
-        return str(os.getenv(var))
-    except KeyError as error:
-        raise MissingEnvironmentVar(f"{var} is missing from environment variables. {error}")
+def get_required(var: str, default: str) -> str:
+    env_var = os.getenv(var, default)
+    if env_var is None:
+        raise MissingEnvironmentVar(f"{var} is missing from environment variables.")
+    return str(env_var)
+        
 
 ##
 ## Project config
 ##
-@dataclass(kw_only=True, slots=True)
+@dataclass(kw_only=True)
 class Config:
     """ Dataclass to hold the DB configurations. Not mutable. """
 
@@ -26,11 +27,11 @@ class Config:
     db_pwd:  Optional[str] = field(init=True, default=None)
 
 def load_config() -> Config:
-    db_host = get_required("DB_HOST")
-    db_port = get_required("DB_PORT")
-    db_name = get_required("DB_NAME")
-    db_user = get_required("DB_USER")
-    db_pwd  = get_required("DB_PWD")
+    db_host = get_required("DB_HOST", "db")
+    db_port = get_required("DB_PORT", "5432")
+    db_name = get_required("DB_NAME", "postgres")
+    db_user = get_required("DB_USER", "admin")
+    db_pwd  = get_required("DB_PWD", "admin")
 
     return Config(
             db_host=db_host, 
